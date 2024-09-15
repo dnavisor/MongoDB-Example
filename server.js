d5d956180cdd667323cba6345145ff04f6a1704b
dnavisor <contact@authoritydev.club>
2024-09-14 20:00:26 -0500

    Pushed

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+mongoose.connect('database_url_string', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// model
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+// ex route
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.status(201).send('User registered');
+  } catch (error) {
+    res.status(500).send('Error registering user');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
